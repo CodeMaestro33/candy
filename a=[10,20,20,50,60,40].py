958465e30d8b3bd1d76b04f1d035d7fbd9f00b3a
CodeMaestro33 <144579360+CodeMaestro33@users.noreply.github.com>
2024-02-17 13:05:05 +0530

    Add files via upload
    The Largest of 3 Number's python

---
+++ a=[10,20,20,50,60,40].py
@@ -0,0 +1,10 @@
+a = int(input("Enter the first number:"))
+b = int(input("Enter the second number:"))
+c = int(input("Enter the third number:"))
+if a>b and a>c:
+    print ("A is the largest") 
+if b>a and b>c:
+    print ("B is the largest")
+if c>a and c>b:
+    print ("C is the largest")       
+       
